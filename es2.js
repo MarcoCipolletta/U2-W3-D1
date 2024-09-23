e835dd38a309da054e4fb8da2cadfbe7a034ffb8
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-09-23 17:02:13 +0200

    Fixed style and species form

--- es2.js
+++ es2.js
@@ -15,6 +15,9 @@ const listOfPet = [];
 const form = document.querySelector("form");
 const petsList = document.getElementById("petsList");
 const list = document.createElement("ul");
+list.classList.add("row");
+list.classList.add("row-cols-6");
+list.classList.add("gap-3");
 
 form.onsubmit = (e) => {
   e.preventDefault();
@@ -31,14 +34,22 @@ form.onsubmit = (e) => {
 
   listOfPet.forEach((pet) => {
     const liItem = document.createElement("li");
+    liItem.classList.add("border");
+    liItem.classList.add("p-3");
+    liItem.classList.add("list-unstyled");
     const listowener = document.createElement("h6");
+    listowener.classList.add("fs-3");
     listowener.innerText = pet.owenerName;
     const listPetName = document.createElement("p");
+    listPetName.classList.add("mb-0");
     listPetName.innerText = pet.petName;
     const listPetSpecies = document.createElement("p");
-    listPetSpecies.innerText = pet.petSpecies;
+    listPetSpecies.classList.add("mb-0");
+    listPetSpecies.innerText = pet.species;
     const listBreed = document.createElement("p");
+    listBreed.classList.add("mb-0");
     listBreed.innerText = pet.breed;
+
     liItem.appendChild(listowener);
     liItem.appendChild(listPetName);
     liItem.appendChild(listPetSpecies);
